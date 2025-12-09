b75eb23767c0798747dcb5a21fe33512b72097d5
laurene-wat <contact@laurene-wat.com>
2025-12-09 11:41:44 +0100

    Update base following domain name change to laurene-wat.com

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => {
     },
     plugins: [react()],
     // IMPORTANT: set the base path for GitHub Pages
-    base: '/landing-page-gemini3/',
+    base: '/',
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
