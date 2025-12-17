1020c7c03b32ba48a388481700ac966c61b6cbb9
laurene-wat <contact@laurene-wat.com>
2025-12-17 12:26:54 +0100

    Update index.tsx - SSG React sans navigateur

--- index.tsx
+++ index.tsx
@@ -3,13 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-// Export pour vite-plugin-ssg: utilisé pendant le build pour générer le HTML
 export const createApp = () => <App />;
 
-// Hydratation côté navigateur
-const rootElement = document.getElementById('root');
-if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(
+const root = document.getElementById('root');
+if (root) {
+  ReactDOM.createRoot(root).render(
     <React.StrictMode>
       <App />
     </React.StrictMode>
